95b91922d9886a43897f63145bd5df5fb6d2c36b
dtgraves <dtgraves@lbl.gov>
2024-07-12 14:33:11 -0700

    got started on computing VTO coefficients on the fly

--- code/src/AmrIce.cpp
+++ code/src/AmrIce.cpp
@@ -1653,6 +1653,7 @@ AmrIce::defineSolver()
                              m_refinement_ratios,
                              dxCrse,
                              m_thicknessIBCPtr,
+                             m_coordSys,
                              numLevels);
 
     m_velSolver = static_cast<IceVelocitySolver*>(solver);
